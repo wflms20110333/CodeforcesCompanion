ec230965378a23a63715993f44f4158edc6001d3
aditya-arjun <aditya.arj1@gmail.com>
2018-09-08 12:44:13 -0500

    backend stuffs

---
+++ cf_api.py
@@ -0,0 +1,100 @@
+#Adapted from yjiao's application of the CF API (https://github.com/yjiao/codeforces-api)
+
+# # Contest Parser v1
+# Goal is to return the following table
+#  
+# handle | rating | contestID | problemID | success 
+# --- | --- | --- | --- | --- | ---
+# handle0 | 1234 | 633 | A | 1
+# handle0 | 1234 | 633 | B | 1
+# handle0 | 1234 | 633 | C | 0
+# 
+
+import os
+import re
+import sys
+import time
+import requests
+import pandas as pd
+
+from collections import defaultdict
+
+
+def getProblemDataFromContest(contestID):
+    url = 'http://codeforces.com/api/contest.standings?contestId=' + str(contestID) + '&from=1&count=1'
+    r = requests.request('GET', url).json()['result']
+
+    contest = r['contest']['name']
+    startTimeSeconds = r['contest']['startTimeSeconds']
+
+    problems = r['problems']
+    probdf = pd.DataFrame.from_dict(problems)
+
+    probdf['contestID'] = contestID
+    probdf['contestName'] = contest
+    probdf['startTimeSeconds'] = startTimeSeconds
+
+    return probdf
+
+
+def getSolveSuccessDF(contestID):
+
+    urlbase = 'http://codeforces.com/api/'
+    method = 'contest.standings'
+    maxcnt = 100000
+    # http://codeforces.com/api/contest.standings?contestId=566&from=1&count=5&showUnofficial=true
+    url = urlbase + method + '?contestId=' + str(contestID) + '&from=1&count=' + str(maxcnt) + '&showUnofficial=false'
+    r = requests.get(url).json()['result']
+    rows = r['rows']
+    problems = r['problems']
+    contest = r['contest']
+
+    # CHECK TO MAKE SURE THAT TEAMS ARE NOT ALLOWED!!!
+    for r in rows:  # for each person
+        if len(r['party']['members']) > 1:
+            return None
+
+    users = []
+    points = []
+    for r in rows:
+        users.append(r['party']['members'][0]['handle'])
+
+        userpts = [0] * len(problems)
+        for i in range(len(problems)):
+            userpts[i] = r['problemResults'][i]['points']
+        points.append([1 if u > 0 else 0 for u in userpts])
+
+    # Grab rating changes
+    method = 'contest.ratingChanges'
+    url = urlbase + method + '?contestId=' + str(contestID)
+    ratingchanges = requests.get(url).json()['result']
+    ratingdict = dict()
+    for r in ratingchanges:
+        ratingdict[r['handle']] = r['oldRating']
+
+    # Create output df
+    # start constructing dataframe
+    array_out = []
+    for i in range(len(users)):  # for each user in the contest
+        hdl = users[i]
+        rating = ratingdict[hdl]
+        for j, p in enumerate(problems):  # for each problem in the contest, make a new row
+            temp = dict.fromkeys(['handle', 'rating', 'contestID', 'problemID', 'success'])
+            temp['handle'] = hdl
+            temp['contestID'] = contestID
+            temp['problemID'] = p['index']
+            temp['success'] = points[i][j]
+            temp['rating'] = rating
+            array_out.append(temp)
+
+    output = pd.DataFrame.from_dict(array_out)
+    return output
+
+
+def getContestList():
+    url_contest = 'http://codeforces.com/api/contest.list?gym=false'
+    r = requests.get(url_contest)  # there are some issues with Russian letters in contest names
+
+    contests = r.json()['result']
+    df_contests = pd.DataFrame.from_dict(contests)
+    return list(df_contests.loc[df_contests.phase == 'FINISHED']['id'])
---
+++ problem_difficulty.py
@@ -0,0 +1,47 @@
+#Adapted from yjiao's application of the CF API (https://github.com/yjiao/codeforces-api)
+import numpy as np
+import cf_api
+
+#Adaptation of Rating Formula from Codeforces (https://codeforces.com/blog/entry/102)
+def get_solve_probability(userRating, problemRating):
+    return 1.0 / (1.0 + np.power(10, (problemRating - userRating) / 400.0))
+
+def save_contest_info():
+    contestList = cf_api.getContestList()
+
+    for ID in contestList:
+        contest_df = get_contest_elo(ID)
+
+def get_contest_elo(contestID):
+    contest_df = cf_api.getSolveSuccessDF(contestID)
+    if(contest_df == None)
+        return None
+
+
+
+
+
+def process_probability(r):
+    return get_solve_probability(r['rating'], r['problemRating'])
+
+MIN_RATING = 0
+MAX_RATING = 4000
+MAX_ITER = 15
+
+def get_problem_elo(problem_df):
+    numSolved = np.sum(problem_df.success)
+
+    lo = MIN_RATING
+    hi = MAX_RATING
+
+    for i in range(MAX_ITER):
+        mid = (lo + hi) / 2.0
+        problem_df['problemRating'] = mid
+        expectedSolves = np.sum(problem_df.apply(process_probability, axis=1))
+
+        if expectedSolves > numSolved:
+            lo = mid
+        else:
+            hi = mid
+
+    return int(round((lo + hi) / 2.0))
