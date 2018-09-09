bf0fa4c0416db493ec8bcedfd618881d442c996c
An Nguyen <annguyencompsci@gmail.com>
2018-09-09 02:21:24 +0000

    he

--- server/applications.py
+++ server/applications.py
@@ -1,11 +1,9 @@
 import helper
-from database import create_connection
+from database import create_connection, search, insert_entry, update_entry
 from flask import Flask, request, jsonify
 import cf_api
 
 app = Flask(__name__)
-conn = create_connection("cf.db")
-db = conn.cursor()
 
 @app.route('/')
 def index():
@@ -14,27 +12,16 @@ def index():
 @app.route('/look')
 def lookup():
     if request.args['contestid'] is None:
-        return 'hi'
+        return jsonify({})
     if request.args['index'] is None:
-        return 'hello'
-    look_id = helper.gen_id(request.args['contestid'], request.args['index'])
-    res = db.execute("SELECT * FROM problems WHERE id = {0}".format(look_id)).fetchall()
-    if not res:
-        return 'ur bad'
-    return jsonify({
-        'contestID' : request.args['contestid'],
-        'index' : request.args['index'],
-        'rating' : res[0][1],
-        'tags' : res[0][2]
-    })
+        return jsonify({})
+    return search(request.args['contestid'], request.args['index'], helper.gen_id(request.args['contestid'], request.args['index']))
 
 @app.route('/insert')
 def insert():
-    if not request.args['contestid'] or not request.args['index'] or not request.args['rating'] or not request.args['tags']:
-        return None
     look_id = helper.gen_id(request.args['contestid'], request.args['index'])
-    res = db.execute("INSERT INTO problems (id, rating, tags) VALUES ({0}, {1}, {2})".format(look_id, request.args['rating'], request.args['tags']))
-    return 'hello'
+    insert_entry(look_id, request.args['rating'], request.args['tags'])
+    return 'hi'
 
 @app.route("/checkHandle")
 def checkHandle():
@@ -42,8 +29,6 @@ def checkHandle():
 
 @app.route('/update')
 def update():
-    if not request.args['contestid'] or not request.args['index'] or not request.args['rating'] or not request.args['tags']:
-        return None
     look_id = helper.gen_id(request.args['contestid'], request.args['index'])
-    res = db.execute("UPDATE INTO problems (id, rating, tags) VALUES ({}, {}, {})".format(
-        look_id, request.args['rating'], request.args['tags']))
+    update_entry(look_id, request.args['rating'], request.args['tags'])
+    return 'hi'
--- server/database.py
+++ server/database.py
@@ -1,10 +1,12 @@
 import sqlite3
 from sqlite3 import Error
+from flask import jsonify
 
 create_problem_table = "CREATE TABLE IF NOT EXISTS problems ("\
     "id INTEGER PRIMARY KEY,"\
+    "problemID INTEGER UNIQUE,"\
     "rating INTEGER,"\
-    "tags BLOB"\
+    "tags TEXT"\
 ");"
 
 def create_connection(filename):
@@ -21,3 +23,45 @@ def create_table(cursor, sql_command):
         cursor.execute(sql_command)
     except Error as e:
         print(e)
+
+def insert_entry(problemID, rating, tags):
+    try:
+        conn = create_connection("cf.db")
+        db = conn.cursor()
+        db.execute('INSERT INTO problems (problemID, rating, tags) VALUES (?,?,?);', (int(problemID), int(rating), str(tags)))
+        conn.commit()
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+def update_entry(problemID, rating, tags):
+    try:
+        conn = create_connection("cf.db")
+        db = conn.cursor()
+        db.execute('UPDATE INTO problems (problemID, rating, tags) VALUES (?,?,?);', (int(problemID), int(rating), str(tags)))
+        conn.commit()
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+def search(contestID, index, problemID):
+    try:
+        conn = create_connection("cf.db")
+        db = conn.cursor()
+        res = db.execute("SELECT * FROM problems WHERE problemID = " + str(problemID)).fetchall()
+        if not res:
+            return jsonify({})
+        conn.close()
+        return jsonify({
+            'contestID' : contestID,
+            'index' : index,
+            'rating' : res[0][2],
+            'tags' : res[0][3]
+        })
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+    return jsonify({})
